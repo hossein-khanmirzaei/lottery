e403f0b3ae9d968aab4a9352062e2bb87640d43e
Hossein Khanmirzaei <hossein.khanmirzaei@gmail.com>
2019-10-09 22:01:42 +0330

    (98=07-17 10:00 PM)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lottery/models/http_exception.dart';
 import 'package:lottery/models/tranaction.dart';
+import 'package:lottery/screens/credit_card_screen.dart';
 import 'package:lottery/widgets/transaction_list.dart';
 import 'package:provider/provider.dart';
 import 'package:lottery/providers/transactions.dart';
+import 'package:lottery/providers/overview.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,6 +13,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _totalCredit = '---';
+  String _totalPayment = '---';
   List<Transaction> _transactions = [];
   var _isLoading = false;
 
@@ -32,6 +36,42 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Future<void> _getTotalCredit() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await Provider.of<OverviewProvider>(context, listen: false)
+          .getTotalCredit('2755533528');
+    } on HttpException catch (error) {
+      //_showErrorDialog(error.toString());
+    } catch (error) {
+      //_showErrorDialog('خطایی رخ داده است. لطفاً بعداً تلاش کنید.');
+    }
+    setState(() {
+      _totalCredit = Provider.of<OverviewProvider>(context).totalCredit;
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _getTotalPayment() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await Provider.of<OverviewProvider>(context, listen: false)
+          .getTotalPayment('2755533528');
+    } on HttpException catch (error) {
+      //_showErrorDialog(error.toString());
+    } catch (error) {
+      //_showErrorDialog('خطایی رخ داده است. لطفاً بعداً تلاش کنید.');
+    }
+    setState(() {
+      _totalPayment = Provider.of<OverviewProvider>(context).totalPayment;
+      _isLoading = false;
+    });
+  }
+
   Future<void> _getTransactionList() async {
     setState(() {
       _isLoading = true;
@@ -47,19 +87,245 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _transactions =
           Provider.of<TransactionProvider>(context, listen: false).transactions;
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
+      _transactions.add(_transactions[0]);
       _isLoading = false;
     });
   }
 
   @override
   void initState() {
+    _getTotalCredit();
+    _getTotalPayment();
     _getTransactionList();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return _isLoading ? Center(child: CircularProgressIndicator()) : TransactionList(_transactions, null);
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          title: Text(""),
+          floating: false,
+          pinned: true,
+          snap: false,
+          expandedHeight: 250,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        'مجموع امتیاز',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                      Text(
+                        'مجموع خرید',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 25.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _totalCredit != null
+                          ? Text(
+                              _totalCredit,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            )
+                          : Icon(
+                              Icons.block,
+                              color: Colors.white54,
+                            ),
+                      _totalPayment != null
+                          ? Text(
+                              _totalPayment,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            )
+                          : Icon(
+                              Icons.block,
+                              color: Colors.white54,
+                            ),
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  ButtonTheme(
+                    minWidth: 120.0,
+                    child: RaisedButton(
+                      child: Text(
+                        "مدیریت کارت",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      color: Colors.green,
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed(CreditCardScreen.routeName);
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return TransactionList(_transactions, null);
+            },
+          ),
+        )
+      ],
+
+      //child: _children[_currentIndex],
+    );
+
+    // return Column(
+    //   children: <Widget>[
+    //     Expanded(
+    //       child: CustomScrollView(
+    //         slivers: <Widget>[
+    //           SliverAppBar(
+    //             title: Text(""),
+    //             floating: false,
+    //             pinned: true,
+    //             snap: false,
+    //             expandedHeight: 250,
+    //             flexibleSpace: FlexibleSpaceBar(
+    //               background: Container(
+    //                 margin: EdgeInsets.only(top: 50),
+    //                 child: Column(
+    //                   mainAxisAlignment: MainAxisAlignment.center,
+    //                   children: <Widget>[
+    //                     Row(
+    //                       mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //                       children: <Widget>[
+    //                         Text(
+    //                           'مجموع امتیاز',
+    //                           textAlign: TextAlign.center,
+    //                           style: TextStyle(
+    //                             color: Colors.white,
+    //                             fontSize: 24.0,
+    //                           ),
+    //                         ),
+    //                         Text(
+    //                           'مجموع خرید',
+    //                           textAlign: TextAlign.center,
+    //                           style: TextStyle(
+    //                             color: Colors.white,
+    //                             fontSize: 24.0,
+    //                           ),
+    //                         ),
+    //                       ],
+    //                     ),
+    //                     SizedBox(height: 25.0),
+    //                     Row(
+    //                       mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //                       children: [
+    //                         _totalCredit != null
+    //                             ? Text(
+    //                                 _totalCredit,
+    //                                 textAlign: TextAlign.center,
+    //                                 style: TextStyle(
+    //                                   color: Colors.white,
+    //                                   fontSize: 20,
+    //                                 ),
+    //                               )
+    //                             : Icon(
+    //                                 Icons.block,
+    //                                 color: Colors.white54,
+    //                               ),
+    //                         _totalPayment != null
+    //                             ? Text(
+    //                                 _totalPayment,
+    //                                 textAlign: TextAlign.center,
+    //                                 style: TextStyle(
+    //                                   color: Colors.white,
+    //                                   fontSize: 20,
+    //                                 ),
+    //                               )
+    //                             : Icon(
+    //                                 Icons.block,
+    //                                 color: Colors.white54,
+    //                               ),
+    //                       ],
+    //                     ),
+    //                     SizedBox(height: 15.0),
+    //                     ButtonTheme(
+    //                       minWidth: 120.0,
+    //                       child: RaisedButton(
+    //                         child: Text(
+    //                           "مدیریت کارت",
+    //                           textAlign: TextAlign.center,
+    //                           style: TextStyle(
+    //                             color: Colors.white,
+    //                             fontSize: 16,
+    //                           ),
+    //                         ),
+    //                         color: Colors.green,
+    //                         onPressed: () {
+    //                           Navigator.of(context)
+    //                               .pushNamed(CreditCardScreen.routeName);
+    //                         },
+    //                         shape: RoundedRectangleBorder(
+    //                           borderRadius: BorderRadius.circular(20.0),
+    //                         ),
+    //                       ),
+    //                     ),
+    //                     SizedBox(height: 15.0),
+    //                   ],
+    //                 ),
+    //               ),
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     _isLoading
+    //         ? Center(child: CircularProgressIndicator())
+    //         : TransactionList(_transactions, null),
+    //   ],
+    // );
+
     // ListView.separated(
     //   itemCount: _transactionList.length,
     //   itemBuilder: (context, index) {
--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:lottery/models/http_exception.dart';
-import 'package:lottery/screens/credit_card_screen.dart';
 import 'package:lottery/screens/gift_screen.dart';
 import 'package:lottery/screens/home_screen.dart';
 import 'package:lottery/screens/shop_screen.dart';
 import 'package:lottery/screens/tranaction_screen.dart';
 import 'package:lottery/widgets/app_drawer.dart';
-import 'package:provider/provider.dart';
-import 'package:lottery/providers/overview.dart';
 
 class OverviewScreen extends StatefulWidget {
   static const routeName = '/overview';
@@ -18,8 +14,6 @@ class OverviewScreen extends StatefulWidget {
 class _OverviewScreenState extends State<OverviewScreen> {
   int _currentIndex = 0;
   // var _isLoading = false;
-  String _totalCredit = '---';
-  String _totalPayment = '---';
   final List<Widget> _children = [
     HomeScreen(),
     TransactionScreen(),
@@ -29,8 +23,6 @@ class _OverviewScreenState extends State<OverviewScreen> {
 
   @override
   void initState() {
-    _getTotalCredit();
-    _getTotalPayment();
     super.initState();
   }
 
@@ -52,42 +44,6 @@ class _OverviewScreenState extends State<OverviewScreen> {
     );
   }
 
-  Future<void> _getTotalCredit() async {
-    // setState(() {
-    //   _isLoading = true;
-    // });
-    try {
-      await Provider.of<OverviewProvider>(context, listen: false)
-          .getTotalCredit('2755533528');
-    } on HttpException catch (error) {
-      //_showErrorDialog(error.toString());
-    } catch (error) {
-      //_showErrorDialog('خطایی رخ داده است. لطفاً بعداً تلاش کنید.');
-    }
-    setState(() {
-      _totalCredit = Provider.of<OverviewProvider>(context).totalCredit;
-      // _isLoading = false;
-    });
-  }
-
-  Future<void> _getTotalPayment() async {
-    // setState(() {
-    //   _isLoading = true;
-    // });
-    try {
-      await Provider.of<OverviewProvider>(context, listen: false)
-          .getTotalPayment('2755533528');
-    } on HttpException catch (error) {
-      //_showErrorDialog(error.toString());
-    } catch (error) {
-      //_showErrorDialog('خطایی رخ داده است. لطفاً بعداً تلاش کنید.');
-    }
-    setState(() {
-      _totalPayment = Provider.of<OverviewProvider>(context).totalPayment;
-      // _isLoading = false;
-    });
-  }
-
   void onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
@@ -96,170 +52,9 @@ class _OverviewScreenState extends State<OverviewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   actions: [
-      //     IconButton(
-      //       icon: Icon(Icons.notifications),
-      //       onPressed: () {},
-      //     ),
-      //   ],
-      //   bottom: PreferredSize(
-      //     preferredSize: Size.fromHeight(150),
-      //     child: Column(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       children: <Widget>[
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //           children: <Widget>[
-      //             Text(
-      //               'مجموع امتیاز',
-      //               textAlign: TextAlign.center,
-      //               style: TextStyle(
-      //                 color: Colors.white,
-      //                 fontSize: 24.0,
-      //               ),
-      //             ),
-      //             Text(
-      //               'مجموع خرید',
-      //               textAlign: TextAlign.center,
-      //               style: TextStyle(
-      //                 color: Colors.white,
-      //                 fontSize: 24.0,
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //         SizedBox(height: 25.0),
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //           children: [
-      //             _totalCredit != null
-      //                 ? Text(
-      //                     _totalCredit,
-      //                     textAlign: TextAlign.center,
-      //                     style: TextStyle(
-      //                       color: Colors.white,
-      //                       fontSize: 20,
-      //                     ),
-      //                   )
-      //                 : Icon(
-      //                     Icons.block,
-      //                     color: Colors.white54,
-      //                   ),
-      //             _totalPayment != null
-      //                 ? Text(
-      //                     _totalPayment,
-      //                     textAlign: TextAlign.center,
-      //                     style: TextStyle(
-      //                       color: Colors.white,
-      //                       fontSize: 20,
-      //                     ),
-      //                   )
-      //                 : Icon(
-      //                     Icons.block,
-      //                     color: Colors.white54,
-      //                   ),
-      //           ],
-      //         ),
-      //         SizedBox(height: 15.0),
-      //         ButtonTheme(
-      //           minWidth: 120.0,
-      //           child: RaisedButton(
-      //             child: Text(
-      //               "مدیریت کارت",
-      //               textAlign: TextAlign.center,
-      //               style: TextStyle(
-      //                 color: Colors.white,
-      //                 fontSize: 16,
-      //               ),
-      //             ),
-      //             color: Colors.green,
-      //             onPressed: () {
-      //               Navigator.of(context).pushNamed(CreditCardScreen.routeName);
-      //             },
-      //             shape: RoundedRectangleBorder(
-      //               borderRadius: BorderRadius.circular(20.0),
-      //             ),
-      //           ),
-      //         ),
-      //         SizedBox(height: 15.0),
-      //       ],
-      //     ),
-      //   ),
-      // ),
-      //drawer: AppDrawer(),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            title: Text("Sample Slivers"),
-            leading: Icon(Icons.menu),
-            backgroundColor: Colors.orangeAccent,
-            floating: false,
-            pinned: true,
-            snap: false,
-          ),
-          
-          SliverList(
-            delegate: SliverChildListDelegate([
-              ListTile(
-                leading: Icon(Icons.volume_off),
-                title: Text("Volume Off"),
-              ),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_mute), title: Text("Volume Mute")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-              ListTile(
-                  leading: Icon(Icons.volume_down), title: Text("Volume Down")),
-            ]),
-          )
-        ],
-
-        //child: _children[_currentIndex],
-      ),
+    return Scaffold(      
+      drawer: AppDrawer(),
+      body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: onTabTapped,
